379d34bccab9f9052d1d9b2ab2e60a71ec8e7fdb
Nguyen Van Toan <toannv@192.168.68.209>
2024-09-17 10:49:30 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,19 @@
+package com.example
+
+import com.example.application.plugins.configureHTTP
+import com.example.application.plugins.configureMonitoring
+import com.example.application.plugins.configureRouting
+import com.example.application.plugins.configureSecurity
+import com.example.application.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
---
+++ src/main/kotlin/com/example/application/plugins/Security.kt
@@ -0,0 +1,7 @@
+package com.example.application.plugins
+
+import io.ktor.server.application.*
+
+fun Application.configureSecurity() {
+
+}
