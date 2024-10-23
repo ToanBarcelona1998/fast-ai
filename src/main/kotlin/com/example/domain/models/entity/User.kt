387895f0a3bc848b516807abdf114c6d65e67b3a
Nguyen Van Toan <toannv@192.168.68.60>
2024-10-23 17:35:05 +0700

    Add: Register response

--- src/main/kotlin/com/example/domain/models/entity/User.kt
+++ src/main/kotlin/com/example/domain/models/entity/User.kt
@@ -1,9 +1,8 @@
-package com.example.domain.models.responds
+package com.example.domain.models.entity
 
 data class User(
-    val id: Int?,
+    val id: Int,
     val userName: String,
-    val email: String,
     val phoneNumber : String?,
     val gender : Int,
     val address: String?,
@@ -17,7 +16,6 @@ data class User(
     fun copyWith(
         id: Int? = this.id,
         userName: String = this.userName,
-        email: String = this.email,
         phoneNumber: String? = this.phoneNumber,
         gender: Int = this.gender,
         address: String? = this.address,
@@ -29,9 +27,8 @@ data class User(
         isActive: Boolean = this.isActive
     ): User {
         return User(
-            id = id,
+            id = id ?: this.id,
             userName = userName,
-            email = email,
             phoneNumber = phoneNumber,
             gender = gender,
             address = address,
--- src/main/kotlin/com/example/domain/models/requests/UserAddRequest.kt
+++ src/main/kotlin/com/example/domain/models/requests/UserAddRequest.kt
@@ -2,7 +2,6 @@ package com.example.domain.models.requests
 
 data class UserAddRequest(
     val userName: String,
-    val email: String,
     val phoneNumber: String?,
     val gender: Int,
     val address: String?,
