3aa615d13a06a4abd4462ba995937db33e578cfa
Nguyen Van Toan <toannv@192.168.68.60>
2024-10-23 11:25:12 +0700

    Update: Account table

--- src/main/kotlin/com/example/database/AccountTable.kt
+++ src/main/kotlin/com/example/database/AccountTable.kt
@@ -1,14 +1,14 @@
 package com.example.database
 
 import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.kotlin.datetime.datetime
+import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
 
 object AccountTable : Table("accounts") {
     val id = integer("id").autoIncrement()
     val email = varchar("email",255).uniqueIndex()
     val password = varchar("password" , 255)
-    val createdAt = datetime("created_at")
-    val updatedAt = datetime("updated_at").nullable()
+    val createdAt = timestamp("created_at")
+    val updatedAt = timestamp("updated_at").nullable()
 
     override val primaryKey = PrimaryKey(id)
 }
