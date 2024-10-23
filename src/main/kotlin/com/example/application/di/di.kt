fec7fb1fa319bdef6de04549bf62a09d38d72bf0
Nguyen Van Toan <toannv@192.168.68.60>
2024-10-23 17:33:08 +0700

    Update: di

--- src/main/kotlin/com/example/application/di/di.kt
+++ src/main/kotlin/com/example/application/di/di.kt
@@ -1,22 +1,27 @@
 package com.example.application.di
 
-import com.example.repository.AuthRepository
+import com.example.repository.AccountRepository
 import com.example.repository.UserRepository
-import com.example.repository.interfaces.IAuthRepository
+import com.example.repository.interfaces.IAccountRepository
 import com.example.repository.interfaces.IUserRepository
+import com.example.services.AccountService
 import com.example.services.AuthService
 import com.example.services.UserService
 import org.koin.dsl.module
 
 val injection = module {
-    single<IAuthRepository> { AuthRepository() }
+    single<IAccountRepository> { AccountRepository() }
 
-    single<IUserRepository> { UserRepository()}
+    single<IUserRepository> { UserRepository() }
 
-    single<AuthService> {
-        AuthService(get<IAuthRepository>())
+    single<AccountService> {
+        AccountService(get<IAccountRepository>())
     }
     single<UserService> {
         UserService(get<IUserRepository>())
+    }
+
+    single<AuthService> {
+        AuthService(get<AccountService>(), get<UserService>())
     }
 }
