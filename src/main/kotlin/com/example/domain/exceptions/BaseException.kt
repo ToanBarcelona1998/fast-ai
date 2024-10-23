e59a4972bf7cdb86f3bcd5b5f9401044760e9391
Nguyen Van Toan <toannv@192.168.68.60>
2024-10-23 17:36:41 +0700

    Add: Added more exceptions

--- src/main/kotlin/com/example/domain/exceptions/BaseException.kt
+++ src/main/kotlin/com/example/domain/exceptions/BaseException.kt
@@ -2,18 +2,34 @@ package com.example.domain.exceptions
 
 class FastAiException(val code: Int, override val message: String) : Exception(message){
     companion object{
-        /// Status code
-        // Auth error code from 1000 - 1010
-        val MISSING_USER_NAME_ERROR_CODE = 1000
+        // - [Error constants] -//
+
+        // Auth error code from 1000 -
+        val MISSING_EMAIL_ERROR_CODE = 1000
         val MISSING_PASSWORD_ERROR_CODE = 1001
+        val EMAIL_INVALID_ERROR_CODE = 1002
+        val ACCOUNT_EXISTS_ERROR_CODE = 1003
 
-        public val UNKNOWN_ERROR_CODE = 9999
-        //
-
-        // Message
-        val MISSING_USER_ERROR_MESSAGE = "Missing user name"
+        // Auth error message
+        val MISSING_EMAIL_ERROR_MESSAGE = "Missing email"
         val MISSING_PASSWORD_ERROR_MESSAGE = "Missing password"
+        val EMAIL_INVALID_ERROR_MESSAGE = "Email is invalid"
+        val ACCOUNT_EXISTS_ERROR_MESSAGE = "Account is exists"
 
+        // User error code from 1010 -
+        val MISSING_USER_NAME_ERROR_CODE = 1010
+        val USER_NOT_FOUND_ERROR_CODE = 1011
+
+        // User error message
+        val MISSING_USER_NAME_ERROR_MESSAGE = "Missing user name"
+        val USER_NOT_FOUND_ERROR_MESSAGE = "User not found"
+
+
+        // Common error code
+        val UNKNOWN_ERROR_CODE = 9999
+        val DATABASE_ERROR_CODE = 10000
+
+        // Common error message
         val SUCCESSFUL_MESSAGE = "Successfully"
         //
     }
