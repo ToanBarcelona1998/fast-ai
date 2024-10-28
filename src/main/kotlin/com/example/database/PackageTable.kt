f6b8422c26dcb1d4dc626f5dceb72e3835f1ab76
Nguyen Van Toan <toannv@192.168.68.60>
2024-10-28 14:22:06 +0700

    Add: Design purchase database

---
+++ src/main/kotlin/com/example/database/PackageTable.kt
@@ -0,0 +1,17 @@
+package com.example.database
+
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
+
+object PackageTable : Table("package") {
+    val id = integer("id").autoIncrement()
+    val name = varchar("name", 255)
+    val credits = integer("credits")
+    val description = varchar("description",255).nullable()
+    val isActive = bool("is_active").default(true)
+    val createdAt = timestamp("created_at")
+    val updatedAt = timestamp("updated_at").nullable()
+    val basePrice = decimal("base_price", 10 ,2 )
+    val promoPrice = decimal("promo_price" , 10, 2).nullable()
+    val promoEndTime = timestamp("promo_end_time").nullable()
+}
