dc2aa098676359f5ea8ebce297f46d989e15375d
Nguyen Van Toan <toannv@192.168.68.60>
2024-10-29 14:22:23 +0700

    Fix: Fixed authenticate user route

--- src/main/kotlin/com/example/routes/User.kt
+++ src/main/kotlin/com/example/routes/User.kt
@@ -2,34 +2,61 @@ package com.example.routes
 
 import com.example.domain.models.BaseResponseSuccessful
 import com.example.services.UserService
+import com.example.utils.claimId
 import com.example.utils.parseErrorToRespond
 import io.ktor.http.*
 import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.userRoutes(userService: UserService) {
     route("/user") {
-        get {
-            try {
-                val principal = call.principal<JWTPrincipal>()
-                val claims = principal?.payload
-                val id = claims?.getClaim("id")?.asInt()
+        authenticate {
+            get {
+                try {
+                    val id = claimId(call)
 
-                val user = userService.getUserById(id)
+                    val user = userService.getUserById(id)
 
-                call.respond(
-                    status = HttpStatusCode.OK,
-                    BaseResponseSuccessful(data = user)
-                )
-            } catch (e: Exception) {
-                parseErrorToRespond(e, call)
+                    call.respond(
+                        status = HttpStatusCode.OK,
+                        BaseResponseSuccessful(data = user)
+                    )
+                } catch (e: Exception) {
+                    parseErrorToRespond(e, call)
+                }
             }
-        }
 
-        post {
+            put {
+                try {
+                    val id = claimId(call)
 
+                    val formData = call.receiveParameters()
+
+                    val userName = formData["user_name"]
+                    val phoneNumber = formData["phone_number"]
+                    val birthday = formData["birthday"]
+                    val address = formData["address"]
+                    val avatar = formData["avatar"]
+                    val status = formData["status"]?.toInt()
+                    val gender = formData["gender"]?.toInt()
+
+                    userService.update(
+                        id = id,
+                        userName = userName,
+                        phoneNumber = phoneNumber,
+                        gender = gender,
+                        birthday = birthday,
+                        address = address,
+                        avatar = avatar,
+                        status = status
+                    )
+
+                } catch (e: Exception) {
+                    parseErrorToRespond(e, call)
+                }
+            }
         }
     }
 }
