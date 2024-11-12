28c81c1a898cad1bab5ee17c5e159e74d6f9391b
Nguyen Van Toan <toannv@192.168.68.117>
2024-11-12 10:36:13 +0700

    Fix: Wrong access token created

--- src/main/kotlin/com/example/services/AuthService.kt
+++ src/main/kotlin/com/example/services/AuthService.kt
@@ -1,20 +1,25 @@
 package com.example.services
 
+import com.example.application.config.ApplicationConfig
 import com.example.application.config.JWTConfig
 import com.example.domain.exceptions.FastAiException
 import com.example.domain.models.responses.LoginResponse
 import com.example.domain.models.responses.RegisterResponse
+import com.example.infrastructure.EmailClient
 import com.example.utils.catchBlockService
+import com.example.utils.generateOTP
 import com.example.utils.verifyPassword
 
-final class AuthService(private val accountService: AccountService ,private val userService: UserService) {
+final class AuthService(private val accountService: AccountService ,private val userService: UserService, private val emailClient: EmailClient) {
     suspend fun register(email : String?, password: String?) : RegisterResponse{
         return catchBlockService {
             val account = accountService.createAccount(email,password)
 
             val user = userService.createUser(email, accountId = account.id, gender = 0, address = null, birthday = null, phoneNumber = null, avatar = null, status = 0)
 
-            val accessToken = JWTConfig.makeJWTToken(userId = user.id)
+            sendVerifyCode(email)
+
+            val accessToken = JWTConfig.makeOnBoardingJWTToken(userId = user.id)
 
             RegisterResponse(accessToken= accessToken)
         }
@@ -34,9 +39,24 @@ final class AuthService(private val accountService: AccountService ,private val 
 
             val user = userService.getUserByAccountId(account.id)
 
-            val accessToken = JWTConfig.makeOnBoardingJWTToken(user.id)
+            val accessToken = JWTConfig.makeJWTToken(user.id)
 
             LoginResponse(accessToken)
+        }
+    }
+
+    private suspend fun sendVerifyCode(email: String?) : Boolean{
+        return catchBlockService {
+
+            if(email.isNullOrEmpty()){
+                throw FastAiException(FastAiException.MISSING_EMAIL_ERROR_CODE, FastAiException.MISSING_EMAIL_ERROR_MESSAGE)
+            }
+
+            val otp = generateOTP()
+
+            val smtpEmail = ApplicationConfig.getSmtpEmail()
+
+            emailClient.sendEmail(to = email , from = smtpEmail , subject = "Fast AI verification" , content = "Your otp code: $otp")
         }
     }
 }
