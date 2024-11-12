3b030731440592dc7f433e6994f15fde1ed42a0d
Nguyen Van Toan <toannv@192.168.68.117>
2024-11-12 11:31:03 +0700

    Add: Add create refresh token

--- src/main/kotlin/com/example/application/config/JWTConfig.kt
+++ src/main/kotlin/com/example/application/config/JWTConfig.kt
@@ -1,38 +1,47 @@
 package com.example.application.config
 
 import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTCreator
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
 import java.util.*
 
-object JWTConfig{
+object JWTConfig {
     private const val SECRET = "fast_ai_secret"
     private const val ISSUER = "fast_ai_issuer"
-    private const val DATETIME = 36_000_00 * 24 // 1 day
+    private const val DATETIME = 36_000_00L * 24 // 1 day
 
     private val algorithm = Algorithm.HMAC256(SECRET)
     val verifier: JWTVerifier = JWT.require(algorithm).withIssuer(ISSUER).build()
 
-    fun makeJWTToken(userId : Int): String{
+    private fun makeJWT(userId: Int, expiredDate: Long): JWTCreator.Builder {
         val now = Date()
-
         return JWT
             .create()
             .withClaim("id", userId)
-            .withExpiresAt(Date(now.time + DATETIME))
+            .withExpiresAt(Date(now.time + expiredDate))
             .withIssuer(ISSUER)
+    }
+
+    fun makeJWTToken(userId: Int): String {
+        return makeJWT(userId, DATETIME)
             .sign(algorithm)
     }
 
-    fun makeOnBoardingJWTToken(userId : Int) : String{
-        val now = Date()
+    fun makeJWTRefreshToken(userId: Int): String {
+        return makeJWT(userId, DATETIME * 30)
+            .sign(algorithm)
+    }
 
-        return JWT
-            .create()
-            .withClaim("id", userId)
-            .withClaim("scope" , "onboarding")
-            .withExpiresAt(Date(now.time + DATETIME))
-            .withIssuer(ISSUER)
+    fun makeOnBoardingJWTToken(userId: Int): String {
+        return makeJWT(userId, DATETIME)
+            .withClaim("scope", "onboarding")
+            .sign(algorithm)
+    }
+
+    fun makeOnBoardingJWTRefreshToken(userId: Int): String {
+        return makeJWT(userId, DATETIME * 30)
+            .withClaim("scope", "onboarding")
             .sign(algorithm)
     }
 }
